cd7b8bedcddb7cad2c9d9871f06df70cf7b4317d
Larissa Paulino <larissaspaulino@gmail.com>
2022-04-20 12:27:32 -0300

    feat/crud

--- crud.sql
+++ crud.sql
@@ -1,34 +1,72 @@
 -- Aqui você deve colocar os códigos SQL referentes às
 -- Simulações de um CRUD
-
 -- Criação
-
 -- 1)
-
-
+INSERT INTO
+    clientes (nome, lealdade)
+VALUES
+    ('Georgia', 0);
 
 -- 2)
-
-
+INSERT INTO
+    pedidos(status, cliente_id)
+VALUES
+    ('Recebido', 6);
 
 -- 3)
-
-
+INSERT INTO
+    produtos_pedidos(pedido_id, produto_id)
+VALUES
+    (6, 1),
+    (6, 2),
+    (6, 6),
+    (6, 8),
+    (6, 8);
 
 -- Leitura
-
 -- 1)
-
-
+SELECT
+    c.id,
+    c.nome,
+    c.lealdade,
+    pe.id,
+    pe.status,
+    pe.cliente_id,
+    pro.id,
+    pro.nome,
+    pro.tipo,
+    pro.preco,
+    pro.pts_de_lealdade
+FROM
+    produtos_pedidos pp
+    JOIN produtos pro ON pro.id = pp.produto_id
+    JOIN pedidos pe ON pp.pedido_id = pe.id
+    JOIN clientes c ON c.id = pe.cliente_id
+WHERE
+    pe.cliente_id = 6;
 
 -- Atualização
-
 -- 1)
-
-
+UPDATE
+    clientes
+SET
+    lealdade = (
+        SELECT
+            SUM(pro.pts_de_lealdade)
+        FROM
+            produtos_pedidos pp
+            JOIN produtos pro ON pro.id = pp.produto_id
+            JOIN pedidos pe ON pp.pedido_id = pe.id
+            JOIN clientes c ON c.id = pe.cliente_id
+        WHERE
+            pe.cliente_id = 6
+    )
+WHERE
+    id = 6 RETURNING *;
 
 -- Deleção
-
 -- 1)
-
-
+DELETE FROM
+    clientes
+WHERE
+    id = 4 RETURNING *;
